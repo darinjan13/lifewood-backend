a84fa16f6a11d1fe40a2d89b38f74c736bbf4f58
Darin Jan Soriano <darinjan13@gmail.com>
2026-03-25 09:25:29 +0800

    fix link for cors

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'https://soriano-lifewood.netlify.app',
       'https://lifewood-eight.vercel.app',
-      'https://lifewood-darinjan13s-projects.vercel.app/'
+      'https://lifewood-darinjan13s-projects.vercel.app'
     ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
